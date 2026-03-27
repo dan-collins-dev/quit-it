0f8759080cf5c639f7e044227c95f137ee4496f2
Dan Collins <dan.collins.dev@gmail.com>
2026-03-26 23:35:55 -0400

    updated styles on modal

--- QuitIt.Web/src/lib/components/home/AddCigaretteModal.svelte
+++ QuitIt.Web/src/lib/components/home/AddCigaretteModal.svelte
@@ -1,6 +1,5 @@
 <script>
-	const { isOpen } = $props();
-
+	import stopIcon from "$lib/assets/stop-palm-svgrepo-com.svg"
 	async function addCigarette() {
 		try {
 			const res = await fetch("http://localhost:5150/api/logs", {
@@ -18,22 +17,61 @@
 	let dialog;
 
 	export function open() {
+		dialog.style.display = "flex";
 		dialog.showModal();
 	}
 
 	function close() {
+		dialog.style.display = "none"
 		dialog.close();
 	}
 </script>
 
 <dialog bind:this={dialog}>
-	<p>Do you really want to smoke that? {isOpen}</p>
-	<button onclick={addCigarette}>Yes</button>
-	<button onclick={() => close()}>No</button>
+	<h2>Do you really want to smoke that?</h2>
+	<div class="logo-container">
+		<img class="logo" src={stopIcon} alt="" srcset="">
+	</div>
+	
+	<button class="confirm-btn" onclick={addCigarette}>Yes</button>
+	<button class="cancel-btn" onclick={() => close()}>No</button>
 </dialog>
 
 <style>
-	p {
-		color: black;
+	img {
+		filter: invert(0);
+	}
+
+	dialog {
+		border: none;
+		display: none;
+		flex-direction: column;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	.confirm-btn {
+		background: linear-gradient(180deg, #ff0000, rgb(143, 0, 0));
+		color: white;
+	}
+
+	.cancel-btn {
+		
+
+		background-color: green;
+		color: white;
+		background: linear-gradient(180deg, #6f8000, #003600);
+	}
+
+	button {
+		font-size: large;
+		border: none;
+		padding: 1rem 2rem;
+		border-radius: 5px;
+		width: 100%
+	}
+
+	h2 {
+		text-align: center;
 	}
 </style>
